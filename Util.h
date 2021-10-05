11da20f283ceb0b83dcce0a85e73dab44cbdf74b
Juan Ignacio Ferrari Destéffaniz <juanignacioferraridesteffaniz@gmail.com>
2021-10-05 18:16:02 -0300

    Util
    5 mentarios

---
+++ Util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H_INCLUDED
+#define UTIL_H_INCLUDED
+
+int isValid(char * n, int * base)
+
+int getValue(char * value)
+
+char isValue(int * n)
+
+int stringLength(char * string)
+
+void separateComma(char * n, char * parteEntera, char * parteFraccionaria)
+
+#endif // UTIL_H_INCLUDED
