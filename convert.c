a7c9acd23d3071125890d310e44e875e27eb3533
Jano Axel Lockhart <janolockhart@gmail.com>
2021-10-10 14:14:31 -0300

    se llama al destructor del registro antes de terminar la ejecucion del main

--- convert.c
+++ convert.c
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
     verificarValores(registroParam);
     cambioDeBaseNumero(registroParam);
 
+    destructor(registroParam);
     exit(EXIT_SUCCESS);
     #else
     ejecutarTest();
